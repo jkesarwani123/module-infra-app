b1040692d9fdd4193a4ad8b5d2c3991ba3876312
Juhi K <jkesarwani.us@gmail.com>
2023-06-01 09:30:47 -0400

    App server instances

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "test" {
-
+  count=length(var.subnet_id)
   ami = "ami-08ee87f57b38db5af"
   instance_type = var.instance_type
-  subnet_id = var.subnet_id
+  subnet_id = var.subnet_id[count.index]
 }
 
