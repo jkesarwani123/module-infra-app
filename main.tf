b48650bc24a2ffe7094783a81c392ca7fc4a1509
Juhi K <jkesarwani.us@gmail.com>
2023-06-01 10:47:50 -0400

    AutoScaling grp

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@
 resource "aws_security_group" "sg" {
   name        = "${var.name}-${var.env}-sg"
   description = "Allow TLS inbound traffic"
-  vpc_id      = var.allow_app_cidr
+  vpc_id      = var.vpc_id
 
   ingress {
     description      = "SSH"
@@ -21,7 +21,7 @@ resource "aws_security_group" "sg" {
     from_port        = 8080
     to_port          = 8080
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
+    cidr_blocks      = var.allow_app_cidr
   }
   egress {
     from_port        = 0
