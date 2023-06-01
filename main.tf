4a08a73e443af309d4e7b95410222eb69033dc59
Juhi K <jkesarwani.us@gmail.com>
2023-06-01 09:53:43 -0400

    AutoScaling grp

--- main.tf
+++ main.tf
@@ -3,13 +3,41 @@
 #  instance_type = var.instance_type
 #  subnet_id = var.subnet_id
 #}
-#
-resource "aws_launch_template" "foobar" {
-  name_prefix   = "${var.name}-${var.env}"
-  image_id      = "ami-1a2b3c"
-  instance_type = "t2.micro"
+# Create Security Group
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
 
+resource "aws_launch_template" "foobar" {
+  name_prefix   = "${var.name}-${var.env}"
+  image_id      = data.aws_ami.ami.id
+  instance_type = var.instance_type
+}
+
+# Create Auto-scaling group
 resource "aws_autoscaling_group" "bar" {
   availability_zones = ["us-east-1a"]
   desired_capacity   = 1
