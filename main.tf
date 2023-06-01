5389155a60158683be4ea8279d7d7081d26926f5
Juhi K <jkesarwani.us@gmail.com>
2023-06-01 10:33:06 -0400

    AutoScaling grp

--- main.tf
+++ main.tf
@@ -4,20 +4,25 @@
 #  subnet_id = var.subnet_id
 #}
 # Create Security Group
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
+resource "aws_security_group" "sg" {
+  name        = "${var.name}-${var.env}-sg"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = var.allow_app_cidr
 
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = var.bastion_cidr
+  }
+  ingress {
+    description      = "APP"
+    from_port        = 8080
+    to_port          = 8080
     protocol         = "tcp"
     cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
-
   egress {
     from_port        = 0
     to_port          = 0
@@ -27,22 +32,23 @@ resource "aws_security_group" "allow_tls" {
   }
 
   tags = {
-    Name = "allow_tls"
+    Name = "${var.name}-${var.env}-sg"
   }
 }
 
-resource "aws_launch_template" "foobar" {
+resource "aws_launch_template" "template" {
   name_prefix   = "${var.name}-${var.env}"
   image_id      = data.aws_ami.ami.id
   instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.sg.id]
 }
 
 # Create Auto-scaling group
-resource "aws_autoscaling_group" "bar" {
+resource "aws_autoscaling_group" "asg" {
   availability_zones = ["us-east-1a"]
-  desired_capacity   = 1
-  max_size           = 1
-  min_size           = 1
+  desired_capacity   = var.desired_capacity
+  max_size           = var.max_size
+  min_size           = var.min_size
 
   launch_template {
     id      = aws_launch_template.foobar.id
