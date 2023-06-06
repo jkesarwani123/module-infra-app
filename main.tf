0fa05bcb473ab578711a64409b7b500ecfe3e7a8
Juhi K <jkesarwani.us@gmail.com>
2023-06-06 19:17:40 -0400

    App Module Load Balancer Listener Rules

--- main.tf
+++ main.tf
@@ -89,3 +89,12 @@ resource "aws_lb_listener_rule" "rule" {
     }
   }
 }
+
+# Create DNS record
+resource "aws_route53_record" "main" {
+  zone_id = var.domain_id
+  name    = local.dns_name
+  type    = "CNAME"
+  ttl     = 30
+  records = [var.lb_dns_name]
+}
