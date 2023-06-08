7d1d22e839f0a2e5f1a37145167b3a5b58bc5d85
Juhi K <jkesarwani.us@gmail.com>
2023-06-08 09:34:05 -0400

    App Infra

--- userdata.sh
+++ userdata.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 labauto ansible
 ansible-pull -i localhost, -U https://github.com/jkesarwani123/roboshop-ansible.git roboshop.yml -e role_name=${name} -e env=${env} &>>/opt/ansible.log
