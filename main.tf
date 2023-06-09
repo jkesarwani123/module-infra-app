9cf60c568db8445f36b90a024b00cf5a2c5d9e02
Juhi K <jkesarwani.us@gmail.com>
2023-06-08 20:13:32 -0400

    Apps

--- main.tf
+++ main.tf
@@ -23,6 +23,13 @@ resource "aws_security_group" "sg" {
     protocol         = "tcp"
     cidr_blocks      = var.allow_app_cidr
   }
+  ingress {
+    description = "PROMETHEUS"
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = var.monitor_cidr
+  }
   egress {
     from_port        = 0
     to_port          = 0
